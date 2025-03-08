e3f6037a1a42ab083bdd7a89627d8f2ea395364b
Murzinov Mikhail <Mikhail.MURZINOV@raiffeisen.ru>
2025-03-08 20:56:49 +0300

    Location value object implementaion

---
+++ core/domain/model/shared_kernel/location.py
@@ -0,0 +1,26 @@
+"""Location value object implementation."""
+
+import random
+
+from pydantic import BaseModel, ConfigDict, Field
+
+
+min_value: int = 1
+max_value: int = 10
+
+
+class Location(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
+    x: int = Field(ge=min_value, le=max_value)
+    y: int = Field(ge=min_value, le=max_value)
+
+    @classmethod
+    def random(cls) -> "Location":
+        return Location(
+            x=random.randint(min_value, max_value),
+            y=random.randint(min_value, max_value),
+        )
+
+    def calc_direction_to(self, target_location: "Location") -> int:
+        return abs(target_location.x - self.x) + abs(target_location.y - self.y)
